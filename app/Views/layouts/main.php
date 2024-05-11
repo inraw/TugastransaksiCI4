a24ab04602d9e4a06088f35c694168a3741b7691
inraw <weyoune97@gmail.com>
2024-05-11 15:21:23 +0700

    tambah balsamiq

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -64,7 +64,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?= base_url('template') ?>/distt/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?= base_url('template') ?>/distt/img/avatar5.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Warni</a>
